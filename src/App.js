da312dfdba449bfeb18cb65851e6303f1e91c02e
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-29 12:24:22 +0600

    finally a fully functional counter has been made

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class Counter extends React.Component {
@@ -7,13 +8,46 @@ class Counter extends React.Component {
     this.state = {
       count: 0
     };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
 
-    this.increment = this.increment.bind(this); 
-    this.decrement = this.decrement.bind(this); 
-    this.reset = this.reset.bind(this); 
+  reset() {
+    this.setState({
+      count: 0
+    });
+  }
+
+  increment() {
+    this.setState((state) => ({
+      count: state.count + 1
+    }));
+  }
+
+  decrement() {
+    this.setState((state) => ({
+      count: state.count - 1
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <button className="inc" onClick={this.increment}>
+          Increment!
+        </button>
+        <button className="dec" onClick={this.decrement}>
+          Decrement!
+        </button>
+        <button className="reset" onClick={this.reset}>
+          Reset
+        </button>
+        <h1>Current Count: {this.state.count}</h1>
+      </div>
+    );
   }
 }
-
 
 function App() {
   return (
@@ -25,9 +59,10 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React By Making A Simple Counter 
+          Learn React By A Simple Counter
         </a>
-
+        
+        <Counter />
       </header>
     </div>
   );
