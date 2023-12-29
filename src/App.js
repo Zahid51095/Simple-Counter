5985fadbe3faa0955174a5560a84ee8afa236fab
Zahid Hasan <zahidhasan140338@gmail.com>
2023-12-29 12:08:44 +0600

    a component named Counter has been created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
 
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+}
 
 
 function App() {
